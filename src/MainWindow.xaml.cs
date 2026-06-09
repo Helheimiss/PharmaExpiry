cc2170c84f15bf72daf0c1db45d817c023d1ac16
Helheimiss <171362259+Helheimiss@users.noreply.github.com>
2026-06-09 19:07:03 +0300

    Update MainWindow.xaml.cs

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Shapes;
 namespace PharmaExpiry
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   just need.                                                                                                                                                                                                                                          .
     /// </summary>
     public partial class MainWindow : Window
     {
